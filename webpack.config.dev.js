f4771bc4ce9caad0c0c88d14ef65bace7427e33d
Shevchenko Viacheslav <shevchenko@MacBook-Pro-Shevchenko.local>
2018-02-04 21:12:14 +0200

    Hot reload

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,17 +1,26 @@
-import path from 'path'
-
+import path from 'path';
+import webpack from 'webpack';
 export default {
-  entry: path.join(__dirname, '/client/index.js'),
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, '/client/index.js')
+  ],
   output: {
     path: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/'
   },
+  plugins: [
+      new webpack.NoEmitOnErrorsPlugin(),
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
     loaders: [
       {
         test: /\.js$/,
         include: path.join(__dirname, 'client'),
-        loaders: [ 'babel-loader' ]
+        loaders: [ 'react-hot-loader/webpack', 'babel-loader' ]
       }
     ]
   },
